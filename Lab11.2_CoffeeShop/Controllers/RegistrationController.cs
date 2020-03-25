d198c2e6dcde25b0655f489435134502d55d6722
Brewer <brandonbrewer@quickenloans.com>
2020-03-25 07:47:07 -0400

    first commit

---
+++ Lab11.2_CoffeeShop/Controllers/RegistrationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab11._2_CoffeeShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("RegistrationIndex");
+        }
+
+        [HttpPost]
+        public IActionResult Registration(string fname, string lname, string email, string phone, string passsword)
+        {
+            ViewData["name"] = fname + " " + lname;
+            ViewData["email"] = email;
+            return View();
+        }
+    }
+}
